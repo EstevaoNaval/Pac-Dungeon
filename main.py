5f13bcb56c66e34a4c7051cec6072e12e1ec8850
Estevão Naval <navalestevao@gmail.com>
2022-05-19 10:00:51 -0300

    Subindo sprites dos montros e cavaleiros. Colocando o background do labirinto 01.

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+from sys import exit
 import pygame
 from pygame.locals import *
 from settings import *
 
 pygame.init()
 
+def draw(self):
+    pass
+
 def main():
     clock = pygame.time.Clock()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption(TITLE)
+
+    background_image = pygame.image.load(r"./map/level_01/tilemap_level_01.png")
     
     running = 1
     while running:
         clock.tick(FPS)
 
+        screen.fill(DARKGREY)
+        screen.blit(background_image, (INITIAL_POSITION_X_GAME,INITIAL_POSITION_Y_GAME))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: exit()
+
         pygame.display.update()
-    
+
     pygame.quit()
 
 if __name__ == '__main__': main()
--- settings.py
+++ settings.py
@@ -2,7 +2,23 @@ from turtle import width
 import pygame
 from pygame.locals import *
 
-FPS = 60
-SCREEN_WIDTH, SCREEN_HEIGHT = 448,576 # 16px X 28 tiles, 16px X 36 tiles 
+# Colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+DARKGREY = (40,40,40)
 
+# Basic game constants
+FPS = 60
+SCREEN_WIDTH, SCREEN_HEIGHT = 448,640 # 16px X 28 tiles, 16px X 36 tiles + 16 X 4 tiles para placar
+INITIAL_POSITION_X_GAME, INITIAL_POSITION_Y_GAME = 0, 32
+TITLE = "Pac-Dungeon"
+BG_COLOR = DARKGREY
+
+# Tile grid
+TILESIZE = 16
+GRID_WIDTH, GRID_HEIGHT = SCREEN_WIDTH/TILESIZE, SCREEN_HEIGHT/TILESIZE
+
+# KNIGHT
 NUM_KNIGHT_HP = 2
+
+# MONSTER
