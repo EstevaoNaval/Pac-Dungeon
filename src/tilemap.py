84f3a56a289305e901570bafceb47e0d822dfe09
Estevão Naval <navalestevao@gmail.com>
2022-06-03 19:54:21 -0300

    Subindo tilemap

---
+++ src/tilemap.py
@@ -0,0 +1,5 @@
+class Tilemap:
+    def __init__(self, path_tilemap_floor, path_tilemap_upper_character, path_tilemap_bottom_character,):
+        self.path_tilemap_floor = path_tilemap_floor
+        self.path_tilemap_upper_character = path_tilemap_upper_character
+        self.path_tilemap_bottom_character = path_tilemap_bottom_character
