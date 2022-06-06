cba8a68f7fb9f399b6b4bf22a9125a65ac598fee
Estevão Naval <navalestevao@gmail.com>
2022-06-06 11:03:12 -0300

    Adicionando sprites, agora direcionados, do hades e ogro

--- src/tilemap.py
+++
@@ -1,5 +0,0 @@
-class Tilemap:
-    def __init__(self, path_tilemap_floor, path_tilemap_upper_character, path_tilemap_bottom_character,):
-        self.path_tilemap_floor = path_tilemap_floor
-        self.path_tilemap_upper_character = path_tilemap_upper_character
-        self.path_tilemap_bottom_character = path_tilemap_bottom_character
